0ae0e3ab0bba8840af52061eff862617140e61f1
mazhoranta <mazhoranta@gmail.com>
2018-09-19 17:49:05 +0300

    write function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zerosCount = 0;
+  for (var i = number; i > 0; i--) {
+    if (i % 5 == 0) {
+      number = i;
+      break;
+    }
+  }
+
+  for (var i = number; i > 0; i-=5) {
+    var k = i;
+    while (k % 5 == 0) {
+      zerosCount++;
+      k = k / 5;
+    }
+  }
+  
+  return zerosCount;
 }
